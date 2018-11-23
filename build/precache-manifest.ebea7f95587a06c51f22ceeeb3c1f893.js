5d0819f81e9babb49a893044449d4b77a6c6d65b
cheekeebreekee <simak_yahor@mail.ru>
2018-11-24 00:41:26 +0300

    add prod build

---
+++ build/precache-manifest.ebea7f95587a06c51f22ceeeb3c1f893.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "16a1911d0ce0ee517001",
+    "url": "/static/js/main.16a1911d.chunk.js"
+  },
+  {
+    "revision": "538929ade72ffedb08c4",
+    "url": "/static/js/1.538929ad.chunk.js"
+  },
+  {
+    "revision": "16a1911d0ce0ee517001",
+    "url": "/static/css/main.bfbd0a3b.chunk.css"
+  },
+  {
+    "revision": "ed1ebc40a7d08c853537081e3fec14d0",
+    "url": "/index.html"
+  }
+];
