b37cebeedcb9795dfa2e7bf53e72b068fe942835
PhilipokTheCat <philipok4596@gmail.com>
2018-11-24 17:34:00 +0300

    belavia-fare-finder/feature: implement calendar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './components/SearchTable'
 import SearchTable from './components/SearchTable';
 import QuantityInput from './components/QuantityInput';
+import Calendar from './components/Calendar';
 
 class App extends Component {
   render() {
     return (
       <main>
         <QuantityInput />
+        <Calendar />
         <SearchTable />
       </main>
     );
--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -8,3 +8,7 @@ export const apiCredentials = {
     "password": "ptLubE4s8arR2cr6",
 };
 export const apiEndpoint = 'https://api-cert.belavia.by/route?Accept=*/*&Cache-control=no-cache&Content-Type=application/json';
+
+export const daysCountBeforeSaleOn = 45;
+export const defaultDateRangeInDays = 90;
+export const daysCountBeforeSaleOff = daysCountBeforeSaleOn + 330;
